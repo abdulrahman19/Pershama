59ea9424b6fdf797c8b0d6263c82c0cedeb9cba4
Abdulrahman Asaad <eng.abdulrahman19@gmail.com>
2018-06-09 06:44:44 +0200

    [21] Composer Autoloading.

--- laracasts/the-php-practitioner/code/core/bootstrap.php
+++ laracasts/the-php-practitioner/code/core/bootstrap.php
@@ -3,11 +3,6 @@ $app = [];
 
 $app['config'] = require 'config.php';
 
-require 'Router.php';
-require 'Request.php';
-require 'core/database/Connection.php';
-require 'core/database/QueryBuilder.php';
-
 $app['database'] = new QueryBuilder(
     Connection::make($app['config']['database'])
 );
